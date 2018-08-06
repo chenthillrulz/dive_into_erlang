9c76fe528fd8267fa3dc5182f9565c6fea8a2e91
Chenthill Palanisamy <chenthill.palanisamy@softwareag.com>
2018-08-06 13:17:06 +0530

    Implement quicksort.

--- quicksort.erl
+++ quicksort.erl
@@ -10,4 +10,15 @@
 -author("chen").
 
 %% API
--export([]).
+-export([quicksort/1]).
+
+quicksort([]) -> [];
+quicksort([Pivot|Tail]) ->
+  {Smaller, Larger} = partition(Pivot, Tail, [], []),
+  quicksort(Smaller) ++ [Pivot] ++ quicksort(Larger).
+
+partition (_, [], Smaller, Larger) -> {Smaller, Larger};
+partition (Pivot, [H|T], Smaller, Larger) ->
+  if H =< Pivot -> partition(Pivot, T, [H|Smaller], Larger);
+    H > Pivot -> partition(Pivot, T, Smaller, [H|Larger])
+  end.
